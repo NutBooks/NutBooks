16d2488969a9008cb381c09d4310609e9265e2ee
cheesecat47 <cheesecat47@gmail.com>
2023-06-18 16:39:50 +0900

    Feat: limit api requests rate

--- api/router/root.go
+++ api/router/root.go
@@ -9,7 +9,10 @@ package router
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/swagger"
+	"log"
+	"time"
 
 	_ "api/docs"
 )
@@ -17,6 +20,12 @@ import (
 func RunServer() {
 
 	app := fiber.New()
+
+	// limit 3 requests per 10 seconds max
+	app.Use(limiter.New(limiter.Config{
+		Expiration: 10 * time.Second,
+		Max:        3,
+	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hi")
@@ -27,5 +36,5 @@ func RunServer() {
 	// https://github.com/swaggo/swag
 	app.Get("/swagger/*", swagger.HandlerDefault)
 
-	app.Listen(":3000")
+	log.Fatal(app.Listen(":3000"))
 }
