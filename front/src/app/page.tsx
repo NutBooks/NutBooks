b9ed780cabb2dd26e817c5b25fd8bbd9c8bae0fc
appletail <csappletail@gmail.com>
2023-07-11 19:31:45 +0900

    [FE] feat: eslint 및 prettier 설정 (#6)
    - prettier
    1. eslint에서 포맷팅을 prettier로 사용하는 eslint-plugin-prettier는 설치안함
    속도 및 오류 발생 가능성 때문에 권장하지 않는다고 하여 eslint 에서
    prettier와 중복되는 부분을 비활성화하는 plugin만 설치
    2. prettier 사용위해 vscode에서 해줘야하는 설정
    format on save 사용
    default formatter를 prettier로 설정
    
    - eslint
    airbnb 규칙으로 설정
    
    - 참고사항
    eslint 및 prettier 참고 사이트
    https://velog.io/@xmun74/Next.js-TS에서-ESLint-Prettier-설정하기#2-prettier-설치

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import { useGetUsersQuery } from "@/redux/services/userApi";
-import { decrement, increment, reset } from "@/redux/features/counterSlice";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useGetUsersQuery } from '@/redux/services/userApi';
+import { decrement, increment, reset } from '@/redux/features/counterSlice';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 
 export default function Home() {
   const count = useAppSelector((state) => state.counterReducer.value);
@@ -11,8 +11,8 @@ export default function Home() {
   const { isLoading, isFetching, data, error } = useGetUsersQuery(null);
 
   return (
-    <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
-      <div style={{ marginBottom: "4rem", textAlign: "center" }}>
+    <main style={{ maxWidth: 1200, marginInline: 'auto', padding: 20 }}>
+      <div style={{ marginBottom: '4rem', textAlign: 'center' }}>
         <h4 style={{ marginBottom: 16 }}>{count}</h4>
         <button onClick={() => dispatch(increment())}>increment</button>
         <button
@@ -31,15 +31,15 @@ export default function Home() {
       ) : data ? (
         <div
           style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr 1fr',
             gap: 20,
           }}
         >
           {data.map((user) => (
             <div
               key={user.id}
-              style={{ border: "1px solid #ccc", textAlign: "center" }}
+              style={{ border: '1px solid #ccc', textAlign: 'center' }}
             >
               <img
                 src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
--- front/src/redux/provider.tsx
+++ front/src/redux/provider.tsx
@@ -1,8 +1,8 @@
 // store컴포넌트가 client-side에 있어야 함을 표시
-"use client";
+'use client';
 
-import { store } from "./store";
-import { Provider } from "react-redux";
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
