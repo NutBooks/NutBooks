e332042d1a0ed2940870514c0f11510fbd4d8981
yeni28 <suntofu0228@naver.com>
2023-08-08 22:42:17 +0900

    [FE] fix : Navbar 콘텐츠 constants 분리 (#41)

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import MenuItems from './MenuItems';
 import Link from 'next/dist/client/link';
+import { INSIGHT, MAIN, LIST, ASK } from '@constants/urlpath';
+import {
+  MAIN_CONTENTS,
+  SECOND_CONTENTS,
+  THIRD_CONTENTS,
+} from '@constants/rootLayout';
 import {
   AiOutlineLogin,
   AiOutlineUserAdd,
@@ -11,24 +17,24 @@ function NavBar() {
   return (
     <nav className="grid grid-cols-3 items-center  px-[6rem]">
       <div>
-        <Link href="/">
+        <Link href={MAIN}>
           <img src="/logo/Nutbooks_logo.png" className="w-[12rem]"></img>
         </Link>
       </div>
       <div className="grid grid-cols-3 font-PreB text-center">
-        <Link href="/">
-          <MenuItems href="/">
-            <span className="text-[1.125rem]">넛북스</span>
+        <Link href={MAIN}>
+          <MenuItems href={MAIN}>
+            <span className="text-[1.125rem]">{MAIN_CONTENTS}</span>
           </MenuItems>
         </Link>
-        <Link href="/insight">
-          <MenuItems href="/insight">
-            <span className="text-[1.125rem]">인사이트</span>
+        <Link href={INSIGHT}>
+          <MenuItems href={INSIGHT}>
+            <span className="text-[1.125rem]">{SECOND_CONTENTS}</span>
           </MenuItems>
         </Link>
-        <Link href="/list">
-          <MenuItems href="/list">
-            <span className="text-[1.125rem]">모아보기</span>
+        <Link href={LIST}>
+          <MenuItems href={LIST}>
+            <span className="text-[1.125rem]">{THIRD_CONTENTS}</span>
           </MenuItems>
         </Link>
       </div>
@@ -41,7 +47,7 @@ function NavBar() {
           <AiOutlineUserAdd className="w-full " size={20} />
           <span className="text-gray-600 text-[.95rem]">회원가입</span>
         </Link>
-        <Link href="/">
+        <Link href={ASK}>
           <div className="items-center text-center">
             <AiOutlineWhatsApp className="w-full" size={20} />
             <span className="text-gray-600 text-[.95rem]">문의하기</span>
---
+++ front/src/constants/rootLayout.ts
@@ -0,0 +1,3 @@
+export const MAIN_CONTENTS = '넛북스';
+export const SECOND_CONTENTS = '인사이트';
+export const THIRD_CONTENTS = '모아보기';
