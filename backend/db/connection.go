5638c4d4c72fb6975ccb13c26f724a9d85e1b177
cheesecat47 <cheesecat47@gmail.com>
2023-08-12 18:00:06 +0900

    [BE] fix: DB 커넥션 풀 제거 복원 (#26,#47)
    - #26에서 커넥션 풀 설정을 했으나, MaxLifetime 이후 커넥션이 close되고 재연결 안되는 문제를 확인.
    - 추후 crud 메소드 실행 시에 DB 커넥션 풀에 연결이 수립된 커넥션이 없다면 새로 연결하는 로직 추가 필요. 다만 이번 이슈(#47) 범위가 아니라 판단되어 현재는 주석처리.

--- backend/db/connection.go
+++ backend/db/connection.go
@@ -1,13 +1,11 @@
 package db
 
 import (
-	"database/sql"
 	"fmt"
+	"github.com/gofiber/fiber/v2/log"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log"
 	"os"
-	"time"
 )
 
 var DB *gorm.DB
@@ -21,28 +19,30 @@ func Connect() {
 		os.Getenv("MYSQL_PORT"),
 		os.Getenv("MYSQL_DATABASE"),
 	)
+	log.Debugw("Connect", "dsn", dsn)
 
 	var err error
-	sqlDB, err := sql.Open("mysql", dsn)
-	if err != nil {
-		log.Panicf("Failed to connect to MySQL: %v", err)
-	}
-	defer sqlDB.Close()
-
-	// https://medium.com/@SlackBeck/golang-database-sql-패키지-삽질기-3편-커넥션-풀-a8c220f7af3d
-	sqlDB.SetMaxIdleConns(10)
-	sqlDB.SetMaxOpenConns(20)
-	sqlDB.SetConnMaxLifetime(time.Hour)
-
-	DB, err = gorm.Open(mysql.New(mysql.Config{
-		Conn: sqlDB,
-	}), &gorm.Config{})
+	//sqlDB, err := sql.Open("mysql", dsn)
+	//if err != nil {
+	//	log.Panicf("Failed to connect to MySQL: %v", err)
+	//}
+	//defer sqlDB.Close()
+	//
+	//// https://medium.com/@SlackBeck/golang-database-sql-패키지-삽질기-3편-커넥션-풀-a8c220f7af3d
+	//sqlDB.SetMaxIdleConns(10)
+	//sqlDB.SetMaxOpenConns(20)
+	//sqlDB.SetConnMaxLifetime(time.Hour)
+	//
+	//DB, err = gorm.Open(mysql.New(mysql.Config{
+	//	Conn: sqlDB,
+	//}), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Panicf("Cannot connect to MySQL: %v", err)
 	}
 
-	log.Println("Connection opened to MySQL")
+	log.Info("Connection opened to MySQL")
 
 	MigrateMysql()
 }
